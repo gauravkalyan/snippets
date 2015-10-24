9923e494f9b9c4f6e3c16ba057a6416ccec1b438
Gaurav Kalyan <gaurav.kalyan2@gmail.com>
2015-10-24 21:54:29 +0530

    Chef Street First Solution

--- code_chef/chef_street.py
+++ code_chef/chef_street.py
@@ -1,11 +1,19 @@
 inputList = [int(x) for x in raw_input().split()]
 street = [int(x) for x in raw_input().split()]
 
-first = inputList[0]%inputList[1]
-product = street[0]%1000000007
-if first >1:
-	product = product*street[first-1]%1000000007
-while(first + inputList[1]<=inputList[0]):
-	first = first + inputList[1]
-	product = product*street[first-1]%1000000007
-print product
+k = inputList[1]
+
+minWeight = []
+minWeight.append(street[0])
+i=1
+while(i<len(street)):
+	weight = 1
+	temp = 0
+	if k < i:
+		subList = minWeight[-k:]
+		temp = min(subList)
+	else:
+		temp = min(minWeight)	
+	minWeight.append(temp*street[i])
+	i+=1
+print minWeight[-1]%1000000007
